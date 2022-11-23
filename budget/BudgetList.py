60e210a8e6e1353eb2aad5fcc852655319f876ab
Razo <razohayon@microsoft.com>
2022-11-23 11:54:16 +0200

    fix next

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -26,9 +26,9 @@ class BudgetList ():
     
     def __next__(self):
         try:
-            return next(self.iter_e)
+            return self.iter_e.__next__()
         except StopIteration as stop:
-            return next(self.iter_o)
+            return self.iter_o.__next__()
             
         
         
